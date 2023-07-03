67bb870bc5b2d68650c5eec79b3346ecb608a86d
cdbhosle <cdbhosle07@gmail.com>
2023-07-03 18:50:34 +0530

    Export/Import

---
+++ csv/src/test/java/com/synoris/CsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synoris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
